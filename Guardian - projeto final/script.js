0bc26c6e2a0aecba36eaa52b69442fd2fab76483
J0ão <jvhenriquegonzaga@gmail.com>
2024-11-08 15:04:35 -0300

    guardian

---
+++ Guardian - projeto final/script.js
@@ -0,0 +1,46 @@
+let lastScrollTop = 0;
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+    const currentScroll = window.pageYOffset;
+
+    if (lastScrollTop < currentScroll  ) {
+        // Esconde o header ao rolar para baixo
+        header.classList.add('visivel');
+    } else {
+        // Mostra o header ao rolar para cima
+        header.classList.remove('visivel');
+    }
+
+   
+
+
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Evita valores negativos
+})
+
+;
+
+
+const setaBaixo = document.querySelector('.seta-baixo');
+let scrollTimeout;
+
+function mostrarSetaDepoisParar() {
+    clearTimeout(scrollTimeout); 
+
+    const limiteInferior = document.documentElement.scrollHeight - window.innerHeight;
+    const isAtEnd = window.scrollY >= limiteInferior - 50; 
+    if (!isAtEnd) {
+        setaBaixo.classList.add('visivel'); 
+    }
+
+    
+    scrollTimeout = setTimeout(() => {
+        if (!isAtEnd) {
+            setaBaixo.classList.remove('visivel'); 
+        }
+    }, 1000);
+}
+
+window.addEventListener('scroll', mostrarSetaDepoisParar);
+
+
